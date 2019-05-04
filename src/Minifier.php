b9226d1be5fcda6ccb154ba9b74af809519afe89
Thomas Peri <tjperi@gmail.com>
2019-05-03 17:10:43 -0700

    improved speed by combining regex parts

--- src/Minifier.php
+++ src/Minifier.php
@@ -24,86 +24,83 @@ class Minifier {
 		return trim(implode('', $tokens));
 	}
 	
-	// Some regex patterns for tokenizing.
-	// Strings represent replacements, true means use the match.
-	private static $patterns = [
+	private static $partials = [
 		// Contiguous whitespace becomes a single space.
-		'#\s+#s' => ' ',
+		'\s+',
 	
 		// The rules for whether a comment is considered nothing or
 		// whitespace are complicated, so for the first pass, just
 		// convert all comments to empty comments.
-		'#(/\*.*?\*/)+#s' => '/**/',
+		'(/\*.*?\*/)+',
 		
 		// Quoted strings stay as they are.
-		'#([\'"])(\\\\?.)*?\1#s' => true,
+		'\'(\\\\?.)*?\'',
+		'\"(\\\\?.)*?\"',
 
 		// Multiple semicolons collapse to a single semicolon.
-		'#(;\s*)+#s' => ';',
+		'(;\s*)+',
 		
 		// Operators in attribute selectors like [class^="foo""]
-		'#[~^|*$]?=#' => true,
+		'[~^|*$]?=',
 		
 		// Any word that precedes an open parentheses,
 		// optionally with a colon to match pseudo-classes.
-		'#:?[\w-]+(?=\()#' => true,
+		':?[\w-]+(?=\()',
 
 		// Isolate a few other characters individually. Keep this one
 		// last so that it doesn't supercede other patterns that begin
 		// with these characters.
-		'#[~^|*$>+\-,:(){}[\]]#' => true,
+		'[~^|*$>+\-,:(){}[\]]',
 	];
 	
 	// Parse the CSS file into an array of tokens.
 	private function tokenize($css) {
+		$all = '#(' . implode(')|(', self::$partials) . ')#s';
+		
 		$tokens = [];
-
 		$len = strlen($css);
 		$offset = 0;
+		$start = 0;
+
 		do {
-			// Set some initial values before each `do` iteration.
-			$match = '';
-			$start = $len;
-			$action = null;
+			$found = preg_match($all, $css, $matches, PREG_OFFSET_CAPTURE, $offset);
+			if ($found) {
+				$match = $matches[0][0];
+				$start = $matches[0][1];
+			} else {
+				$start = $len;
+			}
 			
-			// Match each pattern starting at the offset and use the earliest-occurring one.
-			foreach (self::$patterns as $pattern => $val) {
-				if (
-					preg_match($pattern, $css, $matches, PREG_OFFSET_CAPTURE, $offset) &&
-					$matches[0][1] < $start
-				) {
-					// Update those values when we find a better match.
-					$match = $matches[0][0];
-					$start = $matches[0][1];
-					$action = $val;
-					
-					// No need to keep looking if it's as far back as it can be.
-					if ($start === $offset) {
+			// Add unmatched stuff.
+			if ($start > $offset) {
+				$tokens[] = substr($css, $offset, $start - $offset);
+			}
+			
+			// Add matched stuff.
+			if ($found) {
+				$char = substr($match, 0, 1);
+				$token = $match;
+				switch ($char) {
+					case ';':
+						$token = ';';
 						break;
-					}
+					case '/':
+						if (substr($match, 1, 1) === '*') {
+							$token = '/**/';
+						}
+						break;
+					default:
+						if (preg_match('#\s#', $char)) {
+							$token = ' ';
+						}
 				}
-			}
-
-			// Keep whatever came before the match (or at the end of
-			// the css, if nothing matched).
-			$unmatched = substr($css, $offset, $start - $offset);
-			if ($unmatched !== '') {
-				$tokens[] = $unmatched;
-			}
-			
-			// Process whatever was matched and add it to the result.
-			if (is_string($action)) {
-				$tokens[] = $action;
-
-			} else if ($action === true) {
-				$tokens[] = $match;
+				$tokens[] = $token;
 			}
 			
 			// Move the offset to the end of the match.
 			$offset = $start + strlen($match);
 			
-			// Keep going as long as something was matched.
-		} while ($match !== '');
+		} while ($found);
 		
 		return $tokens;
 	}
@@ -115,7 +112,7 @@ class Minifier {
 		while ($input) {
 			$token = array_pop($input);
 			if ($token === '}') {
-				$empty = $this->blocks_tail($input, $output);
+				$this->blocks_tail($input, $output);
 			} else {
 				$output[] = $token;
 			}
